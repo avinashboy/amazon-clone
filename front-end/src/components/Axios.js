5f240b024fa91d8c7dcbea8f07c3f20c7e8de0a7
avinashboy <52669990+avinashboy@users.noreply.github.com>
2020-09-15 15:28:23 +0530

    Update Axios.js

--- front-end/src/components/Axios.js
+++ front-end/src/components/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'https://api-stripe-paymant-gatways.herokuapp.com'
+  baseURL: 'Add your back-end payment method'
 });
 
 export default instance;
